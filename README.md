284c6fea83d6c1705870e60c9534a86d8382be02
Mavis <mavis.murdock@tufts.edu>
2025-05-20 13:07:24 -0400

    clean up code, add to readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # kinova_starter_code
+
+``direct_control.py``
+- This file lets you directly control the Gen2 arm via an xbox controller. 
+- Note that you can't teleoperate the arm until it has been homed (pass ``homed=True`` to your DirectControl instance). 
+- Before running this code, you will need to get the robot started up. To do that, use the command ``roslaunch kinova_starter_code launch_arm.launch``
+
+### config
+This is where we store the mappings between controllers and the commands that are sent to the robot. ``XYZMode.yaml`` contains the mappings between buttons on an xbox controller and the x/y/z velocity commands sent to the robot.
--- src/direct_control.py
+++ src/direct_control.py
@@ -3,8 +3,7 @@
 import rospy
 import armpy.gen2_teleop
 from teleop_lib.gui.teleop_config_frame import TeleopConfigFrame, get_teleop_info
-from geometry_msgs.msg import Twist
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Twist, PoseStamped
 import numpy as np
 import traceback
 import yaml
@@ -13,69 +12,97 @@ from sensor_msgs.msg import Joy
 import os
 import armpy.gripper
 import kinova_msgs.srv
-                                  
-                           
-class Direct_Control:
-    def __init__(self):
 
+class Direct_Control:
+    """
+    Direct_Control handles joystick-based teleoperation of the Kinova Gen2 robotic arm,
+    including arm velocity control and gripper actuation.
+    """
+
+    def __init__(self, homing=True):
+
+        # Load joystick-to-arm mapping configuration from YAML
         path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../config/XYZMode.yaml")
-        
         with open(path, 'r') as file:
             self.cfg = yaml.safe_load(file)
 
+        # Placeholder for current command
         self.cmd = None
 
+        # Initialize gripper control interface
         self.gripper = armpy.gripper.Gripper()
         
+        # Build joystick input profile based on configuration
         self.mode = teleop_lib.input_profile.build_profile(self.cfg)
 
+        # Placeholder for current pose
         self.pose = PoseStamped()
         
-        self.arm = armpy.gen2_teleop.Gen2Teleop(ns="/j2s7s300_driver", home_arm=False)
-
-    def check_pose(self, data):
-        self.pose = data 
+        # Initialize arm control interface (no homing on startup)
+        self.arm = armpy.gen2_teleop.Gen2Teleop(ns="/j2s7s300_driver", home_arm=homing)
 
     def start(self):
+        """
+        Initializes ROS subscriber for joystick.
+        """
         self.joy_subscriber = rospy.Subscriber("/joy", Joy, self.callback)
-        self.pose_subscriber =rospy.Subscriber("/j2s7s300_driver/out/tool_pose", PoseStamped, self.check_pose)
 
     def callback(self, data):
+        """
+        Main joystick callback to process input and control the arm.
 
-        if data.buttons[4] == 1 and data.buttons[5] == 1: 
-            self.arm.stop()
-            self.open_gripper() 
+        Args:
+            data (Joy): Joystick input message.
+        """
 
-
+        # Get the processed twist command from joystick input
         command = self.mode.process_input(data).twist
+
+        # Create a new Twist message for linear velocity
         new_cmd = Twist()
         new_cmd.linear.x = command.linear.x
         new_cmd.linear.y = command.linear.y
         new_cmd.linear.z = command.linear.z
 
-
+        # Send velocity command to the robotic arm
         self.arm.set_velocity(new_cmd)
 
-
     def open_gripper(self):
+        """
+        Opens the robot gripper.
+        """
         self.gripper.open()
 
     def close_gripper(self):
+        """
+        Closes the robot gripper.
+        """
         self.gripper.close()
 
     def stop_arm(self):
+        """
+        Stops all motion of the robotic arm.
+        """
         self.arm.stop()
 
     def home_arm(self):
+        """
+        Calls the home service to bring the arm to its home position.
+        """
         rospy.wait_for_service("/j2s7s300_driver/in/home_arm")
         home_arm = rospy.ServiceProxy("/j2s7s300_driver/in/home_arm", kinova_msgs.srv.HomeArm)
         home_arm()
 
 if __name__ == "__main__":
+    # Initialize the ROS node
     rospy.init_node('robot_xbox_controller', anonymous=True)
 
-    controller = Direct_Control()
-    controller.home_arm()
-    controller.start()
+    # Create the controller 
+    # Home the arm at startup
+    xbox = Direct_Control(homing=True)
 
+    # Start listening to joystick 
+    xbox.start()
+
+    # Keep the node alive
     rospy.spin()
